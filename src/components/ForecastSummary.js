3774befb1b4cece3be248ec904aa73ef2ce2e430
Danielle Davis <Danielle.s.davis@hotmail.com>
2026-06-10 12:30:44 +0100

    fix: convert ForecastSummary to function declaration

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -2,30 +2,26 @@ import React from "react";
 import PropTypes from "prop-types";
 import WeatherIcon from "react-icons-weather";
 
-const ForecastSummary = (props) => {
+function ForecastSummary(props) {
   const { date, description, icon, temperature } = props;
-
   return (
     <div className="forecast-summary" data-testid="forecast-summary">
       <div className="forecast-summary__date">{date}</div>
       <div className="forecast-summary__icon" data-testid="forecast-icon">
-        {icon}
+        <WeatherIcon name="owm" iconId={icon} />
       </div>
       <div className="forecast-summary__temperature">
         {temperature.max}
         &deg;C
       </div>
       <div className="forecast-summary__description">{description}</div>
-      <div className="forecast-summary_icon" data-testid="forescast-icon">
-        <WeatherIcon name="owm" iconId={icon} />
-      </div>
     </div>
   );
-};
+}
 
 export default ForecastSummary;
 
-ForecastSummary.prototype = {
+ForecastSummary.propTypes = {
   date: PropTypes.number.isRequired,
   description: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired,
