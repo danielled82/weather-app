d25c814be220f9ec57536a79f4b1e65486927a91
Danielle Davis <Danielle.s.davis@hotmail.com>
2026-06-10 13:38:04 +0100

    fix: complete LocationDetails tests

--- src/tests/LocationDetails.test.js
+++ src/tests/LocationDetails.test.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import LocationDetails from "../components/locationDetails";
+import LocationDetails from "../components/LocationDetails";
 
 describe("LocationDetails", () => {
-  it("renders the correct city and location props", () => {
+  it("renders the correct city and country props", () => {
     const { getByText } = render(
-      <LocationDetails city="Manchester" country="UK" />
+      <LocationDetails city="Manchester" country="UK" />,
     );
-
     expect(getByText("Manchester, UK")).toBeInstanceOf(HTMLHeadingElement);
+  });
+
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <LocationDetails city="Manchester" country="UK" />,
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders a different city and country", () => {
+    const { getByText } = render(
+      <LocationDetails city="London" country="UK" />,
+    );
+    expect(getByText("London, UK")).toBeInstanceOf(HTMLHeadingElement);
   });
 });
